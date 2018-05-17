a55b75335ed60d290a4307db1af69c85f690de93
HyunSeokYooon <thdthd6245@gmail.com>
2018-05-17 11:15:51 +0900

    version1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyName from './Counter';
+
+class App extends Component {
+  render() {
+    return (
+      <MyName />
+    );
+  }
+}
+
+export default App;
